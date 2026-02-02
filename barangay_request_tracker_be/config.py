f626dc1cfacc97a1a407ad2b894fe7e6d9adb597
Jan Luis Villanueva <83992203+jlvillanueva@users.noreply.github.com>
2026-02-03 03:40:53 +0800

    included frontend

--- barangay_request_tracker_be/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class DefaultConfig:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///./barangay_requests_tracker.db"
-    DIGEST_SALT = os.getenv("DIGEST_SALT")
-    API_KEY = os.getenv("X_API_KEY")
-    BRGY_CODE = os.getenv("X_BRGY_CODE")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
--- barangay_request_tracker_be/mobile.py
+++
@@ -1,5 +0,0 @@
-from common import app, api
-import resources as apir 
-
-api.add_resource(apir.MobileLogin, '/mobile/MobileLogin')
-api.add_resource(apir.MobileRegistration, '/mobile/MobileRegistration')
